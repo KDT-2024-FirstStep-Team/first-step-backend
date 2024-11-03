5162f2d631f0bb8e65e908533a61fa10c0b016ea
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-11-03 21:36:53 +0900

    feat : S3연동 및 파일 업로드,삭제기능 추가

--- src/main/java/com/kdt/firststep/file/FileController.java
+++ src/main/java/com/kdt/firststep/file/FileController.java
@@ -1,29 +1,28 @@
 package com.kdt.firststep.file;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 @RestController
 @RequestMapping("/file")
 @RequiredArgsConstructor
+@Slf4j
 public class FileController {
 
     private final S3FileService s3FileService;
 
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<UploadResponseDto> uploadFile(@RequestParam("file") MultipartFile file) {
         try {
-            // S3에 파일 업로드 및 URL 반환
-            String fileUrl = s3FileService.upload(file);
-            return ResponseEntity.ok(fileUrl);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload file");
+            return ResponseEntity.ok(s3FileService.upload(file));
+        } catch (Exception e) {
+            log.info(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).
+                    body(new UploadResponseDto(null, "업로드 실패"));
         }
     }
 
@@ -32,10 +31,10 @@ public class FileController {
         try {
             // S3에서 파일 삭제
             s3FileService.delete(fileName);
-            return ResponseEntity.ok("File deleted successfully");
+            return ResponseEntity.ok("파일 삭제 성공");
         } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to delete file");
+            log.info(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("파일 삭제 실패");
         }
     }
 }
