a0d1dfb3f521d8b051696515048285af5c3e8d02
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2024-12-10 19:32:04 +0900

    feat : 상담사 찜 추가, 삭제

---
+++ src/main/java/com/kdt/firststep/counselor/repository/SavedCounselorRepository.java
@@ -0,0 +1,23 @@
+package com.kdt.firststep.counselor.repository;
+
+import com.kdt.firststep.counselor.domain.CounselorProfile;
+import com.kdt.firststep.counselor.domain.SavedCounselor;
+import com.kdt.firststep.user.domain.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SavedCounselorRepository extends JpaRepository<SavedCounselor, Integer> {
+
+    // 특정 유저가 특정 상담사를 찜했는지 확인
+    boolean existsByUserAndCounselorProfile(Users user, CounselorProfile counselorProfile);
+
+    // 특정 유저의 특정 상담사 찜 정보 조회
+    Optional<SavedCounselor> findByUserAndCounselorProfile(Users user, CounselorProfile counselorProfile);
+
+    // 특정 유저의 모든 찜한 상담사 목록 조회 (필요시 사용)
+    List<SavedCounselor> findAllByUser(Users user);
+}
